cb491f3e1a9cf6248790fb8f5ab076d4f4654357
alex2227 <alex2227@hotmail.com>
2023-03-25 15:49:45 -0600

    Added Decagono

--- DECAGONO 2 ESPECIE/DecagSource.cpp
+++ DECAGONO 2 ESPECIE/DecagSource.cpp
@@ -4,22 +4,28 @@
 void init() {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(-100.0, 100.0, -100.00, 100.0);
+	gluOrtho2D(-200.0, 200.0, -200.00, 200.0);
+	
 }
 
-void dibujar_Pentagono()
+void dibujar_Decagono()
 {
-	glBegin(GL_LINE_LOOP);
+	glBegin(GL_LINE_STRIP);
 	glColor3f(1.0, 1.0, 1.0);
 
-	glVertex2f(0, 50);
-	glVertex2f(-47, 16);
-	glVertex2f(-29, -40);
-	glVertex2f(29, -40);
-	glVertex2f(47, 16);
+	glVertex2f(-63, 90);
+	glVertex2f(0, 90);
+	glVertex2f(50, 55);
+	glVertex2f(70, 0);
+	glVertex2f(50, -63);
+	glVertex2f(0, -100);
+	glVertex2f(-60, -100);
+	glVertex2f(-110, -63);
+	glVertex2f(-130, 0);
+	glVertex2f(-110, 53);
+	glVertex2f(-63, 90);
 
 	glEnd();
-
 }
 
 
@@ -27,23 +33,14 @@ void dibujar_Estrella()
 {
 	glBegin(GL_LINE_STRIP);
 	glPointSize(20.0f);
-
 	glColor3f(1.0, 1.0, 1.0);
-
-	glVertex2f(0, 50);
-	glVertex2f(29, -40);
-	glVertex2f(-47, 16);
-	glVertex2f(47, 16);
-	glVertex2f(-29, -40);
-	glVertex2f(0, 50);
-
 	glEnd();
 }
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	dibujar_Pentagono();
+	dibujar_Decagono();
 	dibujar_Estrella();
 	glFlush();
 }
@@ -52,7 +49,7 @@ int main(int argc, char** argv)
 {
 	glutInit(&argc, argv); // inicia la libreria GLUT  
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // configura el modo de video inicial 
-	glutInitWindowSize(640, 580); // Tamaño de la Ventana  
+	glutInitWindowSize(640, 540); // Tamaño de la Ventana  
 	glutInitWindowPosition(500, 250);
 	glutCreateWindow("PENTAGONO  ESPECIE");
 	init();
